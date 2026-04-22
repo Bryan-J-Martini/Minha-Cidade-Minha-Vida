c6d74be77b7ad20823a896adf8f52e87bdc5d612
Thayná Adrielly <124536953+Tags2005@users.noreply.github.com>
2026-04-21 21:51:34 -0300

    Adicionar arquivos de projeto.

---
+++ Views/Home/Cadastro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MCMV.Views.Home
+{
+    public class CadastroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Home/IndexInstituicao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MCMV.Views.Home
+{
+    public class IndexInstituicaoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
